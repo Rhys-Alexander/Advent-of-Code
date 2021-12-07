3f460f2f9357d4166e8090d73efc1c94da1d9c78
Gallahad072 <gallahad072@gmail.com>
2021-12-07 12:37:51 +0000

    improved readability

--- 2021/7.py
+++ 2021/7.py
@@ -9,11 +9,14 @@ def getInput():
 
 # Part 1
 def getFuel(inpt):
+    # Sums list of differences between the median of a list and its elements.
     return int(sum(abs(inpt - median(inpt))))
 
 
 # Part 2
 def getFuelIncrease(inpt):
+    # fuel returns sum of all consecutive numbers up until and including 'd'
     fuel = lambda d: d * (d + 1) / 2
-    sumall = lambda x: sum(fuel(abs(inpt - x(mean(inpt)))))
-    return int(min(sumall(floor), sumall(ceil)))
+    sum_fuel = lambda x: sum(fuel(abs(inpt - x(mean(inpt)))))
+    # gets minimum value of wether the mean was floored or ceiled
+    return int(min(sum_fuel(floor), sum_fuel(ceil)))
