729b4c73979ce4d23955bf66f238311197e797d1
Gallahad072 <gallahad072@gmail.com>
2021-12-15 23:27:09 +0000

    2021 day 15

--- 2021/15.py
+++ 2021/15.py
@@ -14,7 +14,7 @@ def pathfind(inpt):
             return nc
         for x, y in [(nx + a, ny + b) for a, b in ((0, 1), (0, -1), (1, 0), (-1, 0))]:
             if coodInRange((x, y), inpt):
-                new_c = nc + inpt[y][x]
+                new_c = nc + inpt[x][y]
                 if (x, y) in costs and costs[(x, y)] <= new_c:
                     continue
                 costs[(x, y)] = new_c
@@ -22,4 +22,31 @@ def pathfind(inpt):
         q.sort(reverse=True)
 
 
+def enlarge(inpt):
+    blocks = [inpt]
+    while len(blocks) != 9:
+        block = []
+        for line in blocks[-1]:
+            new_line = []
+            for x in line:
+                val = 1 if (x + 1) > 9 else (x + 1)
+                new_line.append(val)
+            block.append(new_line)
+        blocks.append(block)
+
+    enlarged = []
+    for i in range(5):
+        row = blocks[i]
+        for j in range(1, 5):
+            for k, line in enumerate(blocks[i + j]):
+                row[k].extend(line)
+        enlarged.extend(row)
+    return enlarged
+
+
+# Part 1
 print(pathfind(day15()))
+
+
+# Part 2
+print(pathfind(enlarge(day15())))
