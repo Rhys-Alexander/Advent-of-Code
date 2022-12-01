bd8616d17167e581c3f1a3c0de93e79de6eeb934
Gallahad072 <gallahad072@gmail.com>
2022-12-01 10:39:23 +0000

    part 2

--- 2022/1.py
+++ 2022/1.py
@@ -6,3 +6,13 @@ def getMaxCalories(inpt):
 
 
 print(getMaxCalories(day1()))
+
+
+# Part 2
+def getMax3Calories(inpt):
+    return sum(
+        sorted(sum(int(x) for x in elf.split("\n")) for elf in inpt.split("\n\n"))[-3:]
+    )
+
+
+print(getMax3Calories(day1()))
