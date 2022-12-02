1a1d89a4ace4f9800bcef5c6e1e972afad29c0b8
Gallahad072 <gallahad072@gmail.com>
2022-12-02 12:13:20 +0000

    dir change

--- 2022/1.py
+++ 2022/1.py
@@ -2,7 +2,7 @@
 def getMaxCalories():
     return max(
         sum(int(x) for x in elf.split("\n"))
-        for elf in open("1.txt").read().split("\n\n")
+        for elf in open("2022/1.txt").read().split("\n\n")
     )
 
 
@@ -14,7 +14,7 @@ def getMax3Calories():
     return sum(
         sorted(
             sum(int(x) for x in elf.split("\n"))
-            for elf in open("1.txt").read().split("\n\n")
+            for elf in open("2022/1.txt").read().split("\n\n")
         )[-3:]
     )
 
