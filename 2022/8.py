bb02c5bebca26cc3ca0c4f6a0ce21ffa0c7c26a3
Gallahad072 <gallahad072@gmail.com>
2022-12-10 15:03:02 +0000

    part 2

--- 2022/8.py
+++ 2022/8.py
@@ -1,3 +1,5 @@
+import math
+
 grid = [[int(tree) for tree in line.strip()] for line in open("2022/8.txt").readlines()]
 
 # Part 1
@@ -14,3 +16,26 @@ for i in range(1, len(grid) - 1):
             visibleTrees += 1
 
 print(visibleTrees)
+
+# Part 2
+maxScenicScore = 0
+for i in range(1, len(grid) - 1):
+    for j in range(1, len(grid[0]) - 1):
+        scenicScore = 1
+        height = grid[i][j]
+        checks = [
+            reversed(grid[i][:j]),
+            grid[i][j + 1 :],
+            reversed([row[j] for row in grid[:i]]),
+            [row[j] for row in grid[i + 1 :]],
+        ]
+        for check in checks:
+            score = 0
+            for tree in check:
+                score += 1
+                if tree >= height:
+                    break
+            scenicScore *= score
+        maxScenicScore = max(maxScenicScore, scenicScore)
+
+print(maxScenicScore)
