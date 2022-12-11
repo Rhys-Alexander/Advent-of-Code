1dbe1da92e3078ed343eb99a6ae70a2fcec989d0
Gallahad072 <gallahad072@gmail.com>
2022-12-11 21:24:20 +0000

    part 2

--- 2022/10.py
+++ 2022/10.py
@@ -1,7 +1,7 @@
 cycles = []
 x = 1
 
-for line in open("2022/t.txt"):
+for line in open("2022/10.txt"):
     if line.strip() == "noop":
         cycles.append(x)
     else:
@@ -9,4 +9,16 @@ for line in open("2022/t.txt"):
         cycles.append(x)
         x += int(line.split()[1])
 
-print(sum(cycles[i - 1] * (i) for i in range(len(cycles)) if (i - 20) % 40 == 0))
+# Part 1
+print(sum(cycles[i] * (i + 1) for i in range(len(cycles)) if (i - 19) % 40 == 0))
+
+# Part 2
+print(
+    "\n".join(
+        "".join(
+            "#" if j in (x - 1, x, x + 1) else "."
+            for j, x in enumerate(cycles[i * 40 : (i + 1) * 40])
+        )
+        for i in range(len(cycles) // 40)
+    )
+)
