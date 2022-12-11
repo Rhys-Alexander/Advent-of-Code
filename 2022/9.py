54dec02e0433dd85293fc73de025548a9becdad8
Gallahad072 <gallahad072@gmail.com>
2022-12-11 19:40:37 +0000

    part 2

--- 2022/9.py
+++ 2022/9.py
@@ -1,14 +1,31 @@
 instrs = {"U": (0, 1), "D": (0, -1), "L": (-1, 0), "R": (1, 0)}
-h, t = [0, 0], [0, 0]
-positions = set()
 
-for line in open("2022/t.txt"):
-    instr, val = line.strip().split()
-    for i in range(int(val)):
-        h[0] += instrs[instr][0]
-        h[1] += instrs[instr][1]
-        if abs(h[0] - t[0]) > 1 or abs(h[1] - t[1]) > 1:
-            t = [h[0] - instrs[instr][0], h[1] - instrs[instr][1]]
-        positions.add(tuple(t))
 
-print(len(positions))
+def getTailPositions(k):
+    positions = set()
+    for line in open("2022/9.txt"):
+        instr, val = line.strip().split()
+        for i in range(int(val)):
+            k[0][0] += instrs[instr][0]
+            k[0][1] += instrs[instr][1]
+            for i in range(1, len(k)):
+                a, b = k[i - 1][0] - k[i][0], k[i - 1][1] - k[i][1]
+                if abs(a) > 1 or abs(b) > 1:
+                    if a == 0 or b == 0:
+                        k[i][1] += b // 2
+                        k[i][0] += a // 2
+                    else:
+                        k[i][0] += 1 if a > 0 else -1
+                        k[i][1] += 1 if b > 0 else -1
+            positions.add(tuple(k[-1]))
+
+    return len(positions)
+
+
+# Part 1
+k1 = [[0, 0], [0, 0]]
+print(getTailPositions(k1))
+
+# Part 2
+k2 = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]]
+print(getTailPositions(k2))
