6bda03bd081f0c278d370fceadc67cfd7f70633e
Gallahad072 <gallahad072@gmail.com>
2022-12-12 01:30:48 +0000

    cleaned

--- 2022/11.py
+++ 2022/11.py
@@ -9,20 +9,17 @@ def getMonkeyBusiness(rounds, div=1):
         items, op, test, t, f = lines.splitlines()[1:]
         test = int(test.split()[-1])
         x *= test
-        items = [int(item) for item in items[18:].split(",")]
         op = eval(
-            f"lambda old: monkeys[{t[-1]} if (({op[19:]}) // {div} % x ) % {test} == 0 else {f[-1]}][0].append(({op[19:]}) // {div} % x)"
+            f"lambda old: monkeys[{t[-1]} if ({op[19:]} // {div} % x ) % {test} == 0 else {f[-1]}][0].append({op[19:]} // {div} % x)"
         )
-        monkeys.append([items, op, 0])
-
-    for i in range(rounds):
+        monkeys.append([[int(item) for item in items[18:].split(",")], op, 0])
+    for _ in range(rounds):
         for m in monkeys:
             items, op, _ = m
             for item in items:
                 op(item)
             m[-1] += len(items)
             m[0] = []
-
     a, b = sorted(monkey[-1] for monkey in monkeys)[-2:]
     return a * b
 
